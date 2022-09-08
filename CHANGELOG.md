f84af81c9137939ce0e2e3b0c4fa230246f1b97b
vtsindia-dev <73383296+vtsindia-dev@users.noreply.github.com>
2022-09-08 10:24:49 +0530

    code fix

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+## 0.6.2+3
+
+- Suppress Flutter 3 warning about `WidgetsBinding` usage
+
+## 0.6.2+2
+
+- Improve error & timer handling (#709)
+
+## 0.6.2+1
+
+- Add support for webview_flutter@3.0 (#657)
+
+## 0.6.2
+
+- Add support for webview_flutter@2.2 (#625)
+- Replace `evaluateJavascript` with `runJavascriptReturningResult` (#625)
+
+## 0.6.1+3
+
+- Update for Flutter 2.5 (#587)
+
+## 0.6.1+2
+
+- Add support for flutter_widget_from_html_core@0.7.0
+
+## 0.6.0+1
+
+- First release
---
+++ lib/src/web_view/io.dart
@@ -0,0 +1,178 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:webview_flutter/webview_flutter.dart' as lib;
+
+import 'web_view.dart';
+
+class WebViewState extends State<WebView> {
+  final _timers = <Timer>[];
+
+  late double _aspectRatio;
+  String? _firstFinishedUrl;
+  _Issue37? _issue37;
+  lib.WebViewController? _wvc;
+
+  @override
+  void initState() {
+    super.initState();
+    _aspectRatio = widget.aspectRatio;
+
+    if (widget.unsupportedWorkaroundForIssue37) {
+      _issue37 = _Issue37(this);
+      _widgetsBindingInstance?.addObserver(_issue37!);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final webView = _buildWebView();
+
+    if (widget.unsupportedWorkaroundForIssue375 &&
+        defaultTargetPlatform == TargetPlatform.android) {
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          final width = constraints.hasBoundedWidth
+              ? constraints.maxWidth
+              : MediaQuery.of(context).size.width;
+          final height = width / _aspectRatio;
+          return SizedBox(
+            height: min(
+              height,
+              constraints.hasBoundedHeight
+                  ? constraints.maxHeight
+                  : MediaQuery.of(context).size.height,
+            ),
+            width: width,
+            child: webView,
+          );
+        },
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: _aspectRatio,
+      child: webView,
+    );
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+
+    if (widget.unsupportedWorkaroundForIssue37) {
+      _wvc?.reload();
+    }
+  }
+
+  @override
+  void dispose() {
+    for (final timer in _timers) {
+      timer.cancel();
+    }
+
+    if (_issue37 != null) {
+      _widgetsBindingInstance?.removeObserver(_issue37!);
+    }
+
+    super.dispose();
+  }
+
+  Future<String> eval(String js) async {
+    try {
+      return await _wvc!.runJavascriptReturningResult(js);
+    } catch (_) {
+      return '';
+    }
+  }
+
+  Future<void> _autoResize() async {
+    if (!mounted) {
+      return;
+    }
+
+    final evals = await Future.wait([
+      eval('document.body.scrollWidth'),
+      eval('document.body.scrollHeight'),
+    ]);
+    final w = double.tryParse(evals[0]) ?? 0;
+    final h = double.tryParse(evals[1]) ?? 0;
+
+    final r = (h > 0 && w > 0) ? (w / h) : _aspectRatio;
+    final changed = (r - _aspectRatio).abs() > 0.0001;
+    if (changed && mounted) {
+      setState(() => _aspectRatio = r);
+    }
+  }
+
+  Widget _buildWebView() => lib.WebView(
+        debuggingEnabled: widget.debuggingEnabled,
+        initialUrl: widget.url,
+        initialMediaPlaybackPolicy: widget.mediaPlaybackAlwaysAllow
+            ? lib.AutoMediaPlaybackPolicy.always_allow
+            : lib.AutoMediaPlaybackPolicy
+                .require_user_action_for_all_media_types,
+        javascriptMode: widget.js
+            ? lib.JavascriptMode.unrestricted
+            : lib.JavascriptMode.disabled,
+        key: Key(widget.url),
+        navigationDelegate: widget.interceptNavigationRequest != null
+            ? (req) => _interceptNavigationRequest(req)
+            : null,
+        onPageFinished: _onPageFinished,
+        onWebViewCreated: (c) => _wvc = c,
+        userAgent: widget.userAgent,
+      );
+
+  lib.NavigationDecision _interceptNavigationRequest(
+    lib.NavigationRequest req,
+  ) {
+    var intercepted = false;
+
+    if (widget.interceptNavigationRequest != null &&
+        _firstFinishedUrl != null &&
+        req.isForMainFrame &&
+        req.url != widget.url &&
+        req.url != _firstFinishedUrl) {
+      intercepted = widget.interceptNavigationRequest!(req.url);
+    }
+
+    return intercepted
+        ? lib.NavigationDecision.prevent
+        : lib.NavigationDecision.navigate;
+  }
+
+  void _onPageFinished(String url) {
+    _firstFinishedUrl ??= url;
+
+    if (widget.autoResize) {
+      for (final interval in widget.autoResizeIntervals) {
+        if (interval == Duration.zero) {
+          // get dimensions immediately
+          _autoResize();
+        } else {
+          // or wait for the specified duration
+          _timers.add(Timer(interval, _autoResize));
+        }
+      }
+    }
+  }
+}
+
+// TODO: remove workaround when our minimum Flutter version >2.12
+WidgetsBinding? get _widgetsBindingInstance => WidgetsBinding.instance;
+
+class _Issue37 with WidgetsBindingObserver {
+  final WebViewState wvs;
+
+  _Issue37(this.wvs);
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      wvs._wvc?.reload();
+    }
+  }
+}
